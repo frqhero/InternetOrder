bfeb7c84abe6e18c6d07e56d6ef191e4d36d1c96
frqhero <frqhero@gmail.com>
2023-05-16 19:32:18 +0600

    Add tests up to delivery_price field

--- test_internet_order.py
+++ test_internet_order.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 import json
 import unittest
 
@@ -16,6 +17,13 @@ class TestInternetOrder(unittest.TestCase):
         with open('internet_order.json', 'r') as file:
             self.payload = json.load(file)
 
+    def get_response(self):
+        return requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+
     def test_bank_card(self):
         self.payload.pop('bank_card')
         response = requests.post(
@@ -26,7 +34,6 @@ class TestInternetOrder(unittest.TestCase):
         self.assertEqual(
             response.status_code,
             500,
-            'Status code should be 500 as required field is omitted'
         )
         self.assertIn(
             'Отсутствует обязательное поле bank_card',
@@ -41,7 +48,6 @@ class TestInternetOrder(unittest.TestCase):
         self.assertEqual(
             response.status_code,
             500,
-            'Status code should be 500 as bank_card type should be boolean'
         )
         self.assertIn(
             'Тип значения ключа bank_card не булево',
@@ -88,7 +94,263 @@ class TestInternetOrder(unittest.TestCase):
             500,
         )
         self.assertIn(
-            'Имя клиента не должно быть пустым',
+            'customer_name не должно быть пустым',
+            response.text,
+        )
+
+    def test_delivery_address(self):
+        self.payload.pop('delivery_address')
+        response = requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Отсутствует обязательное поле delivery_address',
+            response.text,
+        )
+        self.payload['delivery_address'] = 1
+        response = requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Тип значения ключа delivery_address не строка',
+            response.text,
+        )
+        self.payload['delivery_address'] = ''
+        response = requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'delivery_address не должно быть пустым',
+            response.text,
+        )
+
+    def test_order_date(self):
+        self.payload.pop('order_date')
+        response = requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Отсутствует обязательное поле order_date',
+            response.text,
+        )
+        self.payload['order_date'] = 1
+        response = requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Тип значения ключа order_date не строка',
+            response.text,
+        )
+        self.payload['order_date'] = ''
+        response = requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'order_date не должно быть пустым',
+            response.text,
+        )
+        self.payload['order_date'] = '2023-04-17T21:45:3'
+        response = requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Не удалось order_date преобразовать в дату',
+            response.text,
+        )
+
+    def test_order_number(self):
+        self.payload.pop('order_number')
+        response = requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Отсутствует обязательное поле order_number',
+            response.text,
+        )
+        self.payload['order_number'] = '6'
+        response = requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Тип значения ключа order_number не число',
+            response.text,
+        )
+
+    def test_data(self):
+        self.payload.pop('data')
+        response = requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Отсутствует обязательное поле data',
+            response.text,
+        )
+        self.payload['data'] = '6'
+        response = requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Тип значения ключа data не массив',
+            response.text,
+        )
+        self.payload['data'] = []
+        response = requests.post(
+            self.url,
+            json=self.payload,
+            auth=(self.api_username, self.api_password)
+        )
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Массив data пустой',
+            response.text,
+        )
+
+    def test_series_objects(self):
+        series_object = self.payload['data'][0]
+        series_object_copy = series_object.copy()
+        series_object.pop('series')
+        response = self.get_response()
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Отсутствует обязательное поле series',
+            response.text,
+        )
+        series_object['series'] = series_object_copy['series']
+        series_object.pop('price')
+        response = self.get_response()
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Отсутствует обязательное поле price',
+            response.text,
+        )
+        series_object['price'] = series_object_copy['price']
+        series_object['series'] = 1
+        response = self.get_response()
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Тип значения ключа series не строка',
+            response.text,
+        )
+        series_object['series'] = series_object_copy['series']
+        series_object['price'] = '1'
+        response = self.get_response()
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Тип значения ключа price не число',
+            response.text,
+        )
+        series_object['price'] = series_object_copy['price']
+        series_object['series'] = ''
+        response = self.get_response()
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'series не должно быть пустым',
+            response.text,
+        )
+        series_object['series'] = str(uuid4())
+        response = self.get_response()
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Не найдена серия по гуиду',
+            response.text,
+        )
+
+    def test_delivery_price(self):
+        self.payload['delivery_price'] = ''
+        response = self.get_response()
+        self.assertEqual(
+            response.status_code,
+            500,
+        )
+        self.assertIn(
+            'Тип значения ключа delivery_price не число',
             response.text,
         )
 
